275ce9afe656d1fa6eac1e8e7c8e893cd05cd90a
Carlos Antonio Ceballos Romero <ceballosromerocarlos@gmail.com>
2021-07-19 20:51:31 +0200

    speling variable cidr

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -15,7 +15,7 @@ variable "ami_id" {
   default = {eu-west-1 = "ami-0f377b303df4963ab"}
 }
 
-variable "vpc_cidr" {
+variable "cidr" {
   description = "CIDR block for VPC"
   type        = string
   default     = "10.0.0.0/16"

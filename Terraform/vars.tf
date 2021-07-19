79132a61aadbd0f675c6ea3c3e486b5c46c9532b
Carlos Antonio Ceballos Romero <ceballosromerocarlos@gmail.com>
2021-07-19 20:37:44 +0200

    change vpc

--- Terraform/vars.tf
+++ Terraform/vars.tf
@@ -11,7 +11,7 @@ variable "region" {
 
 # In this part you can add as many ami as you want create
 variable "ami_id" {
- type = "map"
+ type = map
  default = {eu-west-1 = "ami-0f377b303df4963ab"}
 }
 
@@ -25,36 +25,3 @@ variable "key_name" {
  default = "ec2-challenge"
 }
 
-#FOR NETWORK
-
-variable "instance_tenancy" {
- description = "A tenancy option for instances launched into the VPC"
- type = string
- default = "default"
-}
-
-
-variable "enable_dns_hostnames" {
- description = "Should be true to enable DNS hostnames in the VPC"
- type = bool
- default = true
-}
-
-variable "enable_dns_support" {
- description = "Should be true to enable DNS support in the VPC"
- type = bool
- default = true
-}
-
-variable "enable_classiclink" {
- description = "Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic."
- type = bool
- default = false
-}
-
-
-variable "tags" {
- description = "A map of tags to add to all resources"
- type = string
- default = "VPC-CHALLENGE"
-}

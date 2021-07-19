940829c736ce03ffcff05ab0089b97d1639daa43
Carlos Antonio Ceballos Romero <ceballosromerocarlos@gmail.com>
2021-07-19 20:44:58 +0200

    speling

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -2,12 +2,12 @@
 #creation of instance, the ami id, region and instance type is specified in vars.tf
 
 resource "aws_instance" "ADIDAS" {
-  ami           = lookup(var.ami_id, var.region)
-  instance_type = instance_type = "t2.micro"
+  ami = lookup(var.ami_id, var.region)
+  instance_type = "t2.micro"
 
 # Public Subnet assign to instance
-  subnet_id     = aws_subnet.public_1.id
+  subnet_id = aws_subnet.public_1.id
 
 
 # Security group assign to instance
-  vpc_security_group_ids=[aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
